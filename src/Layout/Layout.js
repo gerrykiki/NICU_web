1a684b558ff727188de9f666102ad0988c0638b9
gerry <gerry_pw_lin@wistron.com>
2019-06-14 17:41:51 +0800

    Owner: Gerry_PW_Lin@wistron.com Description: Basic Layout Solution: N/A BugID: N/A

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,8 +1,9 @@
 import { Layout, Icon } from 'antd';
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
-import './Layout.css'
-import Sidermenu from './Sidermenu'
+import './Layout.css';
+import Sidermenu from './Sidermenu';
+import { Radio } from 'antd';
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -10,9 +11,16 @@ const { Header, Content, Footer, Sider } = Layout;
 
 class Layoutframe extends Component {
     state = {
-
+        value: 1,
         collapsed: false,
         mode: 'inline',
+    };
+
+    onChange = e => {
+        console.log('radio checked', e.target.value);
+        this.setState({
+            value: e.target.value,
+        });
     };
 
     toggle = () => {
@@ -21,7 +29,26 @@ class Layoutframe extends Component {
         });
     }
 
-
+    renderSwitch(param) {
+        switch (param) {
+            case 1:
+                return '星期一';
+            case 2:
+                return '星期二';
+            case 3:
+                return '星期三';
+            case 4:
+                return '星期四';
+            case 5:
+                return '星期五';
+            case 6:
+                return '星期六';
+            case 7:
+                return '星期日';
+            default:
+                return param;
+        }
+    }
 
     render() {
         return (
@@ -34,8 +61,8 @@ class Layoutframe extends Component {
                     <div className="logo" />
                     <Sidermenu />
                 </Sider>
-                <Layout>
-                    <Header style={{ background: '#000', padding: 0 }}>
+                <Layout style={{ background: '#fff' }}>
+                    <Header style={{ background: '#000', padding: 0, display: 'flex' }}>
                         <span style={{ color: '#fff', paddingLeft: '2%', fontSize: '1.4em' }}>
                             <Icon
                                 className="trigger"
@@ -44,19 +71,26 @@ class Layoutframe extends Component {
                                 style={{ cursor: 'pointer' }}
                             />
                         </span>
-                        <span style={{ color: '#fff', position: 'absolute', left: '50%', marginLeft: '100px', fontSize: '1.4em' }}>病房總覽</span>
-                        <span style={{ color: '#fff', float: 'right', paddingRight: '2%' }}>Hi,陳國榮醫師 {new Date().toLocaleDateString()}</span>
+                        <span style={{ color: '#fff', fontSize: '1.4em', margin: 'auto' }}>病房總覽</span>
+                        <span style={{ color: '#fff', float: 'right', paddingRight: '2%' }}>Hi,陳國榮醫師 {new Date().toLocaleDateString()} {this.renderSwitch(new Date().getDay())}</span>
                     </Header>
                     <Content style={{ margin: '10px 16px' }}>
-                        <div style={{ background: '#333', height: '50px' }}>
-                            <span style={{ color: '#fff', position: 'absolute', left: '50%', marginLeft: '100px', fontSize: '1.4em' }}>病房總覽</span>
-                            <span style={{ color: '#fff', float: 'right', paddingRight: '1%' }}>Hi,陳國榮醫師 {new Date().toLocaleDateString()}</span>
+                        <div style={{ background: '#fff', height: '50px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                            <Radio.Group style={{ marginLeft: 20 }} onChange={this.onChange} value={this.state.value}>
+                                <Radio style={{ color: '#000' }} value={1}>簡易顯示</Radio>
+                                <Radio style={{ color: '#000' }} value={2}>詳細顯示</Radio>
+                            </Radio.Group>
+                            <span style={{ color: '#000', width: 'auto', marginRight: 20 }}>資料更新時間 {new Date().toLocaleDateString()}</span>
                         </div>
-                        <div style={{ marginTop: 20, padding: 24, background: '#fff', minHeight: '1000px' }}>
+                        <div style={{ padding: 1, background: '#fff', minHeight: '1000px',display:'flex' }}>
+                            <div style={{ background: '#333', height: '1000', margin: '10px',flexGrow:1}}>
+                            </div>
+                            <div style={{ background: '#333', height: '1000px', margin: '10px',flexGrow:2}}>
+                            </div>
                         </div>
                     </Content>
                     <Footer style={{ textAlign: 'center' }}>
-                        Ant Design ©2016 Created by Ant UED
+                        SWPC MRT300 Create by NICU group
         </Footer>
                 </Layout>
             </Layout>
