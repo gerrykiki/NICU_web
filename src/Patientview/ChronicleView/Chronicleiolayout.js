ae693bf24a8a4056b2089349561650d7ed91d2aa
Gerry PW Lin <gerry_pw_lin@wistron.com>
2019-08-24 16:39:38 +0800

    Owner: Gerry_PW_Lin@wistron.com Description: update chronic view Solution: N/A BugID: N/A

--- src/Patientview/ChronicleView/Chronicleiolayout.js
+++ src/Patientview/ChronicleView/Chronicleiolayout.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 
 class Chronicleio extends Component {
+    state = {
+        itemNumber: "day"
+    }
 
-
-
+    switchitemNumber() {
+        switch (this.state.itemNumber) {
+            case "day":
+                return "2fr repeat(24,1fr)"
+            case "threeday":
+                return "2fr repeat(21,1fr)"
+            case "week":
+                return "2fr repeat(21,1fr)"
+            default:
+                return "2fr repeat(14,1fr)"
+        }
+    }
     render() {
-        const IOdata = []
+        const intercalstyle = {
+            display: "grid",
+            gridTemplateColumns: this.switchitemNumber(),
+            height: "50px"
+        }
+        const ivsource = []
+        for (let index = 0; index < 24; index++) {
+            const element = Math.floor(Math.random() * 100)
+            ivsource.push(element)
+        }
+        
+        let IVdata = ivsource.map(
+            (item, index) =>
+                <div key={index} style={{fontSize:"16px",textAlign:"center",lineHeight:"50px"}}>{item}</div>
+        )
         return (
             <div style={{ width: "100%", marginBottom: "20px" }}>
                 <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '40px' }}>
@@ -13,8 +40,29 @@ class Chronicleio extends Component {
                         I/O
                     </div>
                 </div>
-                <div style={{ display: "grid", gridTemplateColumns: "2fr repeat(21,1fr)", height: "50px" }}>
+                <div style={intercalstyle}>
                     <div style={{ backgroundColor: "rgba(255,249,237,1)", fontSize: "16px", textAlign: "center", lineHeight: "50px" }}>IV</div>
+                    {IVdata}
+                </div>
+                <div style={intercalstyle}>
+                    <div style={{ backgroundColor: "rgba(255,249,237,1)", fontSize: "16px", textAlign: "center", lineHeight: "50px" }}>Feed</div>
+                    {IVdata}
+                </div>
+                <div style={intercalstyle}>
+                    <div style={{ backgroundColor: "rgba(255,249,237,1)", fontSize: "16px", textAlign: "center", lineHeight: "50px" }}>Other</div>
+                    {IVdata}
+                </div>
+                <div style={intercalstyle}>
+                    <div style={{ backgroundColor: "rgba(255,249,237,1)", fontSize: "16px", textAlign: "center", lineHeight: "50px" }}>Input</div>
+                    {IVdata}
+                </div>
+                <div style={intercalstyle}>
+                    <div style={{ backgroundColor: "rgba(255,249,237,1)", fontSize: "16px", textAlign: "center", lineHeight: "50px" }}>Urine</div>
+                    {IVdata}
+                </div>
+                <div style={intercalstyle}>
+                    <div style={{ backgroundColor: "rgba(255,249,237,1)", fontSize: "16px", textAlign: "center", lineHeight: "50px" }}>Output</div>
+                    {IVdata}
                 </div>
             </div>
         );
